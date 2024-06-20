a500a39f4984bc027cdaa53f577b88f480b4bdd0
GaelVM <ajhuneor@outlook.com>
2024-06-20 00:01:53 -0500

    aaa

--- scrap.java
+++ scrap.java
@@ -1,31 +1,32 @@
+// Importa las clases necesarias de Jsoup
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-
 public class scrap {
-
     public static void main(String[] args) {
         String url = "https://moonani.com/PokeList/";
 
         try {
-            // Realizar la petición HTTP y obtener el HTML de la página
+            // Establece la configuración para encontrar la ubicación del archivo JAR de Jsoup
+            System.setProperty("java.class.path", "lib/jsoup-<version>.jar");
+
+            // Realiza la petición HTTP y obtén el HTML de la página
             Document doc = Jsoup.connect(url).get();
 
-            // Ejemplo: Obtener el título de la página
+            // Ejemplo: Obtén el título de la página
             String title = doc.title();
             System.out.println("Título de la página: " + title);
 
-            // Ejemplo: Obtener todos los enlaces de la página
+            // Ejemplo: Obtén todos los enlaces de la página
             Elements links = doc.select("a[href]");
             System.out.println("Enlaces encontrados:");
             for (Element link : links) {
                 System.out.println(link.attr("href"));
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
